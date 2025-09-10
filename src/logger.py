8038c3e8d1b13e0720a3d27704169a36bd3c7d1f
Rudra Tyagi <rudratyagi777@gmail.com>
2025-09-10 18:51:39 +0530

    updata logger,data ingestion and .gitignore

--- src/logger.py
+++ src/logger.py
@@ -3,20 +3,19 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m-%d-%Y_%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(os.path.dirname(logs_path), exist_ok=True)
 
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOGS_DIR = os.path.join(os.getcwd(), "logs")  # folder for logs
 
+os.makedirs(LOGS_DIR, exist_ok=True)  # make folder if not exists
+
+LOG_FILE_PATH = os.path.join(LOGS_DIR, LOG_FILE)  # full file path
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(levelname)s - %(message)s",
     level=logging.INFO,
-
-) 
-
+)
 
 if __name__ == "__main__":
     logging.info("Logging has started.")
